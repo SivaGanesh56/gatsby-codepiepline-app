f125312e1d2f0afee6a5214415dbb552c35b8a5d
Siva Ganesh Manyam <siva.manyam@siva.manyam-JXFV0M7QKM>
2023-06-12 19:50:32 +0530

    aws sdk added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require(`path`);
 const fs = require("fs");
+const AWS = require("aws-sdk");
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
@@ -25,4 +26,25 @@ exports.createPages = async ({ actions, graphql }) => {
       },
     });
   });
+};
+
+exports.onPostBuild = async ({}) => {
+  const s3 = new AWS.S3({});
+
+  try {
+    const data = {
+      time: new Date().toString(),
+    };
+
+    await s3
+      .putObject({
+        Bucket: "gatsbysitepoc",
+        Key: "outputFile.json",
+        Body: JSON.stringify(data),
+      })
+      .promise();
+    console.log("Successfully uploaded data to bucket");
+  } catch (error) {
+    console.log("error on s3 operation", error);
+  }
 };
