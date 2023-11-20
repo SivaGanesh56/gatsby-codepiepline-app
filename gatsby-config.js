8d8ecb9b884dafe73fd3cb6158bb3a8a89ef2b20
Siva Ganesh Manyam <siva.manyam@siva.manyam-JXFV0M7QKM>
2023-11-20 14:16:32 +0530

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,32 @@
-require("dotenv").config({
-  path: `.env`,
-});
+// require("dotenv").config({
+//   path: `.env`,
+// });
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
-  siteMetadata: {
-    title: `siva's poc`,
-    siteUrl: `https://www.sivaganesh.com`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-env-variables`,
-      options: {
-        allowList: ["MY_APP_KEY"],
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.GATSBY_SPACE_ID,
-        accessToken: process.env.GATSBY_ACCESS_TOKEN,
-        environment: "master",
-        downloadLocal: false,
-        pageLimit: 100,
-      },
-    },
-    `gatsby-plugin-image`,
-  ],
-};
+// /**
+//  * @type {import('gatsby').GatsbyConfig}
+//  */
+// module.exports = {
+//   siteMetadata: {
+//     title: `siva's poc`,
+//     siteUrl: `https://www.sivaganesh.com`,
+//   },
+//   plugins: [
+//     {
+//       resolve: `gatsby-plugin-env-variables`,
+//       options: {
+//         allowList: ["MY_APP_KEY"],
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-contentful`,
+//       options: {
+//         spaceId: process.env.GATSBY_SPACE_ID,
+//         accessToken: process.env.GATSBY_ACCESS_TOKEN,
+//         environment: "master",
+//         downloadLocal: false,
+//         pageLimit: 100,
+//       },
+//     },
+//     `gatsby-plugin-image`,
+//   ],
+// };
