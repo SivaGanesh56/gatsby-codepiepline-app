117fefd519b8cf617854e5ddd6c817a0fa850fc4
Siva Ganesh Manyam <siva.manyam@siva.manyam-JXFV0M7QKM>
2023-11-20 13:39:15 +0530

    buildspec fixs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,55 @@
-const path = require(`path`);
-const fs = require("fs");
-const AWS = require("aws-sdk");
+// const path = require(`path`);
+// const fs = require("fs");
+// const AWS = require("aws-sdk");
 
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = async ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  const { data = [] } = await graphql(`
-    {
-      allContentfulPage {
-        nodes {
-          title
-        }
-      }
-    }
-  `);
+//   const { data = [] } = await graphql(`
+//     {
+//       allContentfulPage {
+//         nodes {
+//           title
+//         }
+//       }
+//     }
+//   `);
 
-  const PageTemplate = path.resolve(`src/templates/Page.jsx`);
+//   const PageTemplate = path.resolve(`src/templates/Page.jsx`);
 
-  data.allContentfulPage.nodes.forEach((item) => {
-    createPage({
-      component: PageTemplate,
-      path: `pages/${item.title}`,
-      context: {
-        ...item,
-      },
-    });
-  });
-};
+//   data.allContentfulPage.nodes.forEach((item) => {
+//     createPage({
+//       component: PageTemplate,
+//       path: `pages/${item.title}`,
+//       context: {
+//         ...item,
+//       },
+//     });
+//   });
+// };
 
-exports.onPostBuild = async ({}) => {
-  const { S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY } = process.env;
-  const s3 = new AWS.S3({
-    accessKeyId: S3_ACCESS_KEY,
-    secretAccessKey: S3_SECRET_ACCESS_KEY,
-  });
+// exports.onPostBuild = async ({}) => {
+//   const { S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY } = process.env;
+//   const s3 = new AWS.S3({
+//     accessKeyId: S3_ACCESS_KEY,
+//     secretAccessKey: S3_SECRET_ACCESS_KEY,
+//   });
 
-  try {
-    const data = {
-      time: new Date().toString(),
-      key: "value",
-    };
+//   try {
+//     const data = {
+//       time: new Date().toString(),
+//       key: "value",
+//     };
 
-    await s3
-      .putObject({
-        Bucket: "testbucket5356",
-        Key: "outputFile.json",
-        Body: JSON.stringify(data),
-      })
-      .promise();
-    console.log("Successfully uploaded data to bucket");
-  } catch (error) {
-    console.log("error on s3 operation", error);
-  }
-};
+//     await s3
+//       .putObject({
+//         Bucket: "testbucket5356",
+//         Key: "outputFile.json",
+//         Body: JSON.stringify(data),
+//       })
+//       .promise();
+//     console.log("Successfully uploaded data to bucket");
+//   } catch (error) {
+//     console.log("error on s3 operation", error);
+//   }
+// };
